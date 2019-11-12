dfb229d61e4ed320a8b4fbf17d045853580b696f
guth1bindu <56920122+guth1bindu@users.noreply.github.com>
2019-11-12 16:26:26 +0530

    Add files via upload

---
+++ sss.py
@@ -0,0 +1,9 @@
+import logging
+logging.basicConfig(filename="log.txt",level=logging.WARNING)
+print("loging demo")
+logging.debug("debug information")
+logging.info("info information")
+logging.warning("worning information")
+logging.error("error information")
+logging.critical("critical information")
+    
