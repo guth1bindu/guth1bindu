237a19227ac644e7908393b5d5cca53fa0ac3914
guth1bindu <56920122+guth1bindu@users.noreply.github.com>
2019-11-12 16:46:00 +0530

    Add files via upload

---
+++ sss1.py
@@ -0,0 +1,8 @@
+import logging
+logging.basicConfig(filename="bindu.txt",level="INFO")
+print("loggoing demo")
+logging.debug("debug information")
+logging.info("info information")
+logging.warning("warning information")
+logging.error("error information")
+logging.critical("critical information")
